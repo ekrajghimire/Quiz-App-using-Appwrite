895f0be6423f201e6a62d54fc6d7ef730ce4fb4e
Ekraj Ghimire <56933759+ManishGhimire127@users.noreply.github.com>
2024-08-22 22:33:55 +0530

    layouts

--- lib/main.dart
+++ lib/main.dart
@@ -21,17 +21,70 @@ class _QuizAppState extends State<QuizApp> {
         title: const Text('Quiz App'),
         backgroundColor: Colors.blueAccent.shade400,
       ),
-      body: Column(
-        children: [
-          // For Question
-          Container(
-            height: 100,
-            child: const Text(
-              'Qn.',
-              style: TextStyle(fontSize: 24.0),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            // For Question
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.blue.shade50,
+                  borderRadius: BorderRadius.circular(5)),
+              height: 100,
+              width: double.infinity,
+              child: const Center(
+                child: Text(
+                  'Qn.',
+                  style: TextStyle(fontSize: 24.0, color: Colors.black),
+                ),
+              ),
             ),
-          )
-        ],
+
+            //opt1
+            MaterialButton(
+              color: Colors.blue,
+              minWidth: double.infinity,
+              onPressed: () {},
+              child: const Text(
+                'Opt 1',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+
+            //opt2
+            MaterialButton(
+              color: Colors.blue,
+              minWidth: double.infinity,
+              onPressed: () {},
+              child: const Text(
+                'Opt 2',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+
+            //opt3
+            MaterialButton(
+              color: Colors.blue,
+              minWidth: double.infinity,
+              onPressed: () {},
+              child: const Text(
+                'Opt 3',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+
+            //opt4
+            MaterialButton(
+              color: Colors.blue,
+              minWidth: double.infinity,
+              onPressed: () {},
+              child: const Text(
+                'Opt 4',
+                style: TextStyle(fontSize: 18),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
