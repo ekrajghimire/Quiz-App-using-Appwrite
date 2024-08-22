15443dc50dd4c16e4f685fa9663b0b3f8612bb8d
Ekraj Ghimire <56933759+ManishGhimire127@users.noreply.github.com>
2024-08-23 00:32:24 +0530

    qn and options added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:quizapp/model.dart';
 import 'package:http/http.dart' as http;
@@ -25,10 +27,19 @@ class _QuizAppState extends State<QuizApp> {
   Future getData() async {
     String url =
         'https://cloud.appwrite.io/v1/databases/66c2e402003d6341b865/collections/66c2e41200346c92a802/documents';
-    var response = await http.get(Uri.parse(url));
+    var response = await http.get(Uri.parse(url), headers: {
+      'X-Appwrite-Project': '66c2e3d6000d2068f7d0',
+      'X-Appwrite-Key':
+          '858534f96891d94826e35af93f14b94288a9945113e8f17de87bd0b8120412b902bd6347d0517669d691ff65f385ad8b0d6f7d9474a277d7da4e2fd237f00093c6ad2bce4cf1d0fc1f42d86d2b46fdf0f929c95e87d5b3edf6583e3816570eb40b866e3e3c92429700d979d10d13e0423b219213d60e4cf4873b7ce8b2181ff3',
+    });
 
     if (response.statusCode == 200) {
-      print(response.body);
+      setState(() {
+        for (var item in jsonDecode(response.body)['documents']) {
+          quizes.add(Quiz(item['title'], item['option1'], item['option2'],
+              item['option3'], item['option4'], item['correctoption']));
+        }
+      });
     }
   }
 
@@ -51,79 +62,84 @@ class _QuizAppState extends State<QuizApp> {
         backgroundColor: Colors.blueAccent.shade400,
         child: const Icon(Icons.refresh),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            // For Question
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.blue.shade50,
-                  borderRadius: BorderRadius.circular(5)),
-              height: 100,
-              width: double.infinity,
-              child: const Center(
-                child: Text(
-                  'Qn.',
-                  style: TextStyle(fontSize: 24.0, color: Colors.black),
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
+      body: quizes.isNotEmpty
+          ? Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  // For Question
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.blue.shade50,
+                        borderRadius: BorderRadius.circular(5)),
+                    height: 100,
+                    width: double.infinity,
+                    child: Center(
+                      child: Text(
+                        'Qn. ${quizes[currentQuestion].title}',
+                        style: const TextStyle(
+                            fontSize: 24.0, color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
 
-            //opt1
-            MaterialButton(
-              color: Colors.blue,
-              minWidth: double.infinity,
-              height: 50,
-              onPressed: () {},
-              child: const Text(
-                'Opt 1',
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-            const SizedBox(height: 10),
+                  //opt1
+                  MaterialButton(
+                    color: Colors.blue,
+                    minWidth: double.infinity,
+                    height: 50,
+                    onPressed: () {},
+                    child: Text(
+                      'A. ${quizes[currentQuestion].option1}',
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
 
-            //opt2
-            MaterialButton(
-              color: Colors.blue,
-              minWidth: double.infinity,
-              height: 50,
-              onPressed: () {},
-              child: const Text(
-                'Opt 2',
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-            const SizedBox(height: 10),
+                  //opt2
+                  MaterialButton(
+                    color: Colors.blue,
+                    minWidth: double.infinity,
+                    height: 50,
+                    onPressed: () {},
+                    child: Text(
+                      'B. ${quizes[currentQuestion].option2}',
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
 
-            //opt3
-            MaterialButton(
-              color: Colors.blue,
-              minWidth: double.infinity,
-              height: 50,
-              onPressed: () {},
-              child: const Text(
-                'Opt 3',
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-            const SizedBox(height: 10),
+                  //opt3
+                  MaterialButton(
+                    color: Colors.blue,
+                    minWidth: double.infinity,
+                    height: 50,
+                    onPressed: () {},
+                    child: Text(
+                      'C. ${quizes[currentQuestion].option3}',
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
 
-            //opt4
-            MaterialButton(
-              color: Colors.blue,
-              minWidth: double.infinity,
-              height: 50,
-              onPressed: () {},
-              child: const Text(
-                'Opt 4',
-                style: TextStyle(fontSize: 18),
+                  //opt4
+                  MaterialButton(
+                    color: Colors.blue,
+                    minWidth: double.infinity,
+                    height: 50,
+                    onPressed: () {},
+                    child: Text(
+                      'D. ${quizes[currentQuestion].option4}',
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                  )
+                ],
               ),
             )
-          ],
-        ),
-      ),
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
     );
   }
 }
